dc03761e707dbd69b9bedbd7b7c5bdf2d7181ff1
rodreego <rodrigogarcia.vix@gmail.com>
2019-05-05 01:47:55 -0300

    primeiro commit

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,21 @@
+class WhatsAppController{
+
+    constructor(){
+
+        console.log("WhatsAppController OK");
+
+        this.loadElements();
+
+    }
+
+    loadElements(){
+
+        this.el = {};
+
+        document.querySelectorAll('[id]').forEach(element =>{
+
+            this.el[Format.getCamelCase(element.id)] = element;
+
+        });
+    }
+}
