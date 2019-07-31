031965f60264a753339bd9d422cb3023b74830d9
rodreego <rodrigogarcia.vix@gmail.com>
2019-07-31 01:11:47 -0300

    camera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,4 +1,4 @@
-class CameraController{
+export class CameraController{
 
     constructor(videoEL){
 
@@ -8,9 +8,11 @@ class CameraController{
 
             video: true
 
-        }).then(screenStream=>{
+        }).then(stream=>{
 
-            this._videoEl.srcObject = screenStream;
+            this._stream = stream;
+            
+            this._videoEl.srcObject = stream;
 
             this._videoEl.play();
 
@@ -19,6 +21,32 @@ class CameraController{
             console.error(err);
 
         });
+
+    }
+
+    stop(){
+
+        this._stream.getTracks().forEach(track =>{
+
+            track.stop();
+
+        });
+
+    }
+
+    takePicture(mimeType = 'image/png'){
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
 
     }
 
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,4 +1,8 @@
-class WhatsAppController{
+import {Format} from './../util/Format';
+
+import {CameraController} from './CameraController';
+
+export class WhatsAppController{
 
     constructor(){
 
@@ -227,12 +231,46 @@ class WhatsAppController{
 
             this.el.panelMessagesContainer.show();
 
+            this._camera.stop();
+
         });
 
         this.el.btnTakePicture.on('click', e=>{
 
-            console.log('take picture');
+            let dataUrl = this._camera.takePicture();            
 
+            this.el.pictureCamera.src = dataUrl;
+
+            this.el.pictureCamera.show();
+
+            this.el.videoCamera.hide();
+
+            this.el.btnReshootPanelCamera.show();
+
+            this.el.containerTakePicture.hide();
+
+            this.el.containerSendPicture.show();
+
+        });
+
+        this.el.btnReshootPanelCamera.on('click', e=>{
+
+            this.el.pictureCamera.hide();
+
+            this.el.videoCamera.show();
+
+            this.el.btnReshootPanelCamera.hide();
+
+            this.el.containerTakePicture.show();
+
+            this.el.containerSendPicture.hide();
+
+        });
+
+        this.el.btnSendPicture.on('click', e=>{
+
+            console.log(this.el.pictureCamera.src);
+            
         });
 
         this.el.btnAttachDocument.on('click', e=>{
