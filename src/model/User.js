f96b47887e50f8a024ecbfc3838740f291e16aa7
rodreego <rodrigogarcia.vix@gmail.com>
2019-11-01 00:03:09 -0300

    alteracoes

--- src/model/User.js
+++ src/model/User.js
@@ -1,8 +1,49 @@
 import { Firebase } from './../util/Firebase';
 
-import { ClassEvent } from '../util/ClassEvent';
 
-export class User extends ClassEvent{
+import { Model} from './Model';
+
+export class User extends Model{
+
+    constructor(id){
+
+        super();
+
+        
+
+        if(id) this.getById(id);
+    }
+
+    get name(){return this._data.name; }
+    set name(value){this._data.name = value}
+
+    get email(){return this._data.email; }
+    set name(value){this._data.email = value}
+
+    get name(){return this._data.photo; }
+    set name(value){this._data.photo = value}
+
+
+
+    getById(id){
+
+        return new Promise((s,f)=>{
+
+            User.findByEmail(id).onSnapshot(doc =>{
+
+                this.fromJSON(doc.data());
+
+                s(doc); 
+
+            });            
+            
+        });
+    }
+
+    save(){
+        
+        return User.findByEmail(this.email).set(this.toJSON());
+    }
 
     static getRef(){
 
