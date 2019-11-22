d103cb8216edadb16488b797842b1633e77bc870
rodreego <rodrigogarcia.vix@gmail.com>
2019-11-22 00:39:35 -0300

    mudança

--- src/model/Model.js
+++ src/model/Model.js
@@ -10,7 +10,7 @@ export class Model extends ClassEvent{
     fromJSON(json){
 
         this._data = Object.assign(this._data, json);
-        this.trigger('datachange', this._toJSON());
+        this.trigger('datachange', this.toJSON());
 
     }
     
